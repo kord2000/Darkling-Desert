43f6eab3e0f3986a4bd7a1df870a54a94c83929c
Andrew Balbuena <abalbuena2000@gmail.com>
2026-06-19 01:52:25 -0400

    Refactored the boundary setup for cleaner reading of initialization of boundary sprites.

--- scripts/gamePieces/Boundary.js
+++ scripts/gamePieces/Boundary.js
@@ -3,7 +3,6 @@ Boundary Class: Contains the player, enemies, chests, etc.
 */
 
 export class Boundary {
-
   /*
   Boundary Constructor - Initializes the q5play group class for boundary sprites. 
   group: Initializes the q5play group class creating a boundaries group. 
@@ -11,7 +10,7 @@ export class Boundary {
   group.stroke: Sets the color for the groups border. 
   group.physics: Sets the physics for the broundary group. 
   group.h: Sets the height of each boundary. 
-  */  
+  */
   constructor(q) {
     this.group = new q.Group();
     this.group.color = "purple";
@@ -23,29 +22,39 @@ export class Boundary {
   setup - creates the barriers for the game screen. Creates openings for enemies to spawn from. 
   */
   setup = (q) => {
+    let thirdWidth = q.width / 3;
+    let sixthWidth = q.width / 6;
+
+    let thirdHeight = q.height / 3;
+    let sixthHeight = q.height / 6;
+
     // Top
-    let boundaryTWR = new this.group.Sprite(q.width / 6, 5);
-    let boundaryTWL = new this.group.Sprite((5 * q.width) / 6, 5);
-    boundaryTWR.w = q.width / 3;
-    boundaryTWL.w = q.width / 3;
+    let boundaryTWR = this.createBoundary(sixthWidth, 5, thirdWidth, 0);
+    let boundaryTWL = this.createBoundary(5 * sixthWidth, 5, thirdWidth, 0);
+
     // Bottom
-    let boundaryBWR = new this.group.Sprite(q.width / 6, q.height - 5);
-    let boundaryBWL = new this.group.Sprite((5 * q.width) / 6, q.height - 5);
-    boundaryBWR.w = q.width / 3;
-    boundaryBWL.w = q.width / 3;
+    let boundaryBWR = this.createBoundary(sixthWidth, q.height - 5, thirdWidth, 0);
+    let boundaryBWL = this.createBoundary(5 * sixthWidth, q.height - 5, thirdWidth, 0);
+
     // Left
-    let boundaryTHL = new this.group.Sprite(5, q.height / 6);
-    let boundaryBHL = new this.group.Sprite(5, (5 * q.height) / 6);
-    boundaryTHL.w = q.height / 3;
-    boundaryTHL.rotation = 90;
-    boundaryBHL.w = q.height / 3;
-    boundaryBHL.rotation = 90;
+    let boundaryTHL = this.createBoundary(5, sixthHeight, thirdHeight, 90);
+    let boundaryBHL = this.createBoundary(5, 5 * sixthHeight, thirdHeight, 90);
     // Right
-    let boundaryTHR = new this.group.Sprite(q.width - 5, q.height / 6);
-    let boundaryBHR = new this.group.Sprite(q.width - 5, (5 * q.height) / 6);
-    boundaryTHR.w = q.height / 3;
-    boundaryTHR.rotation = 90;
-    boundaryBHR.w = q.height / 3;
-    boundaryBHR.rotation = 90;
-  }
+    let boundaryTHR = this.createBoundary(q.width - 5, sixthHeight, thirdHeight, 90);
+    let boundaryBHR = this.createBoundary(q.width - 5, 5 * sixthHeight, thirdHeight, 90);
+  };
+
+  /*
+  createBoundary - Helper function for creating the boundary sprite. 
+  x - Define the sprites x-position. 
+  y - Define the sprites y-position. 
+  width - Define the sprites width. 
+  rotation - Define the sprites rotation. 
+  */
+  createBoundary = (x, y, width, rotation) => {
+    let boundary = new this.group.Sprite(x, y);
+    boundary.w = width;
+    boundary.rotation = rotation;
+    return boundary;
+  };
 }
